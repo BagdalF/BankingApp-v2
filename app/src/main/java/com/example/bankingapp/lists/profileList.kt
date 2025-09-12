2369943dba2a78fe891273d483c22cb313ac0a1b
FELIPEOLIVEIRABAGDAL <bagdalfelipe@gmail.com>
2025-09-12 10:36:37 -0300

    (Em andamento) Edicao de Perfil Funcional

---
+++ app/src/main/java/com/example/bankingapp/lists/profileList.kt
@@ -0,0 +1,7 @@
+package com.example.bankingapp.lists
+
+import com.example.bankingapp.controllers.ProfileData
+
+// Mutable list to store user profiles
+val profileList: MutableList<ProfileData> = mutableListOf()
+
